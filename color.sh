8c263fe2f3e44fbd37af30753bdba0541e5f47be
RusticBard <sanket.p.kumbhar.2805@gmail.com>
2024-07-03 06:00:25 +0530

    changing the colors of the folder

---
+++ color.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+color=$1
+dimensions=(16 22 24 32 48 64 72 96 128 192 256 512)
+
+for dimension in ${dimensions[@]}
+do
+	cp ./${dimension}x${dimension}/places/folder-${color}.png ./${dimension}x${dimension}/places/folder.png
+done
+
