f15c089e9487dc67d34c1c8fac9d48be0dade2a3
RusticBard <sanket.p.kumbhar.2805@gmail.com>
2024-07-03 14:26:03 +0530

    Switching back to svg with customizable folder colors

--- change-color.sh
+++ change-color.sh
@@ -3,11 +3,19 @@
 colorName=$1
 colorNameDark="${colorName}-dark"
 
-color=$(awk -v vawk="${1}" '$1==vawk{print $3}' colors.txt)
-colorDark=$(awk -v vawk="${1}-dark" '$1==vawk{print $3}' colors.txt)
-
-sed -i "64s/#[a-f0-9]\{6\}/#${color::-2}/g" ./scalable/places/folder.svg
-sed -i "59s/#[a-f0-9]\{6\}/#${colorDark::-2}/g" ./scalable/places/folder.svg
+if [ ${colorName} = 'reset' ]; then
+	cp ./scalable/places/folder-grey.svg ./scalable/places/folder.svg
+else
+    if grep -wq "${colorName}" ./colors.txt ; then 
+        color=$(awk -v vawk="${1}" '$1==vawk{print $3}' colors.txt)
+        colorDark=$(awk -v vawk="${1}-dark" '$1==vawk{print $3}' colors.txt)
+        
+        sed -i "64s/#[a-f0-9]\{6\}/#${color::-2}/g" ./scalable/places/folder.svg
+        sed -i "59s/#[a-f0-9]\{6\}/#${colorDark::-2}/g" ./scalable/places/folder.svg
+    else 
+        echo "Invalid argument ..."
+    fi
+fi
 
 gsettings set org.gnome.desktop.interface icon-theme 'Hicolor'
 gsettings set org.gnome.desktop.interface icon-theme 'Skeuowaita'
